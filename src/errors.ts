a688bb9723737003a1fb2e13fe2f290eb18de800
yesui <yesui@shimo.im>
2022-07-08 18:18:33 +0800

    fix: custom errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,13 +1,35 @@
 import { Context } from './shimo-broadcast-channel'
 
 export class MessageError extends Error {
+  name = 'MessageError'
   originMessage: unknown
   context: Context
+
+  constructor (message: string) {
+    super(message)
+
+    Object.setPrototypeOf(this, MessageTimeoutError.prototype)
+  }
 }
 
-export class MessageTimeoutError extends Error {}
+export class MessageTimeoutError extends Error {
+  name = 'MessageTimeoutError'
+
+  constructor (message: string) {
+    super(message)
+
+    Object.setPrototypeOf(this, MessageTimeoutError.prototype)
+  }
+}
 
 export class InvokeError extends Error {
+  name = 'InvokeError'
   method: string
   arguments: unknown[]
+
+  constructor (message: string) {
+    super(message)
+
+    Object.setPrototypeOf(this, MessageTimeoutError.prototype)
+  }
 }
